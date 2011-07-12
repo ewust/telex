2639ac2a2dc5d5121e38eee91f6979e11a590d34
Eric <eric@hobocomp.com>
2011-07-12 01:17:41 -0400

    adding prereq install script to ease install from source

---
+++ telex-client/prereq.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+TELEX_HOME=`pwd`
+cd $TELEX_HOME
+
+wget http://www.openssl.org/source/openssl-1.0.0d.tar.gz
+wget http://monkey.org/~provos/libevent-2.0.12-stable.tar.gz
+wget http://prdownloads.sourceforge.net/argtable/argtable2-13.tar.gz
+    
+# Setting up:
+    mkdir -p $TELEX_HOME/req/local
+    cd $TELEX_HOME/req
+
+# Building required libraries:
+
+    # patched OpenSSL
+    cd $TELEX_HOME/req/
+    tar xf ../openssl-1.0.0d.tar.gz
+    cd openssl-1.0.0d
+    patch -p1 < ../../openssl-1.0.0d-telex.patch
+    ./config --shared --prefix=$TELEX_HOME/req/local/ --openssldir=$TELEX_HOME/req/local/
+    make
+    make install # no root (sudo) needed for local install
+
+    # libevent2, linked against patched OpenSSL
+    cd $TELEX_HOME/req/
+    tar xf ../libevent-2.0.12-stable.tar.gz 
+    cd libevent-2.0.12-stable
+    LDFLAGS="-L$TELEX_HOME/req/local/lib" && export LDFLAGS
+    CFLAGS="-I$TELEX_HOME/req/local/include" && export CFLAGS
+    ./configure --prefix=$TELEX_HOME/req/local
+    make
+    make install 
+
+    # argtable 2 (libargtable2-dev)
+    cd $TELEX_HOME/req/
+    tar xf ../argtable2-13.tar.gz
+    cd argtable2-13
+    ./configure --prefix=$TELEX_HOME/req/local
+    make
+    make install
+    
+# Building telex-client:
+#cd $TELEX_HOMEi/
+#make
+# Running telex-client: 
+#LD_LIBRARY_PATH=$TELEX_HOME/req/local/lib ./telex-client notblocked.telex.cc
